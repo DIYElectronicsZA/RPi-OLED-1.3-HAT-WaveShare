a8298f9159a32bae8a3f657ec8f9e80b7f1433bb
Luca Ruggier <lucaruggier111@gmail.com>
2019-09-02 13:34:33 +0200

    Update setup.sh
    and readme :^)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+sudo wget https://raw.githubusercontent.com/DIYElectronicsZA/RPi-OLED-1.3-HAT-WaveShare/master/Python3.tar.gz
+sudo tar -xzf Python3.tar.gz
+cd Python3
 sudo apt-get update --allow-releaseinfo-change
 sudo apt-get upgrade -y
 sudo apt-get install python3-dev python3-pip libffi-dev libssl-dev -y
@@ -5,8 +8,6 @@ sudo -H pip3 install --upgrade pip
 sudo apt-get purge python3-pip
 sudo -H pip3 install --upgrade luma.oled
 sudo pip3 install smbus
-sudo raspi-config -> Interfacing Options -> P4 SPI -> Yes -> OK -> Finish
-sudo reboot now    (THEN WAIT and reconnect over SSH)
 sudo apt-get install python3-numpy
 sudo apt-get install libopenjp2-7
 sudo apt install libtiff5
